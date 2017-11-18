46f240a9ba62bb5dcd3209f3ae40c31e14b1adb7
Ryoga Gushiken <e175749@ie.u-ryukyu.ac.jp>
2017-11-18 13:27:27 +0900

    first commit

--- src/Null.java
+++ src/Null.java
@@ -1,11 +1,6 @@
 public class Null {
     public static void main(String[] args){
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }catch (Exception e){
-            System.out.println("NullPointExceptionが発生");
-            System.out.println(e.getMessage());
-        }
+        String str="3．14";
+        double value=Double.parseDouble(str);
     }
 }
